a4c3dd2b195ccd7be76f11d4044525fbb57a3733
Mike Pagé <mikepagenl@gmail.com>
2016-07-05 17:27:00 +0200

    Update Joomla module add search form

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,15 +1,17 @@
-<div id="recranet-container" data-eq-pts="small: <?php echo $params->get('breakpointSmall'); ?>, medium: <?php echo $params->get('breakpointMedium'); ?>, large: <?php echo $params->get('breakpointLarge'); ?>">
+<div id="recranet<?php echo ($params->get('view') !== 'default' ? '-' . $params->get('view') : ''); ?>-container" data-eq-pts="small: <?php echo $params->get('breakpointSmall'); ?>, medium: <?php echo $params->get('breakpointMedium'); ?>, large: <?php echo $params->get('breakpointLarge'); ?>">
     <ng-view autoscroll="true"></ng-view>
 </div>
 
-<script type="text/javascript">
-    var recranetConfig = <?php echo json_encode($recranetConfig); ?>;
+<?php if ($params->get('sdk')) : ?>
+    <script type="text/javascript">
+        var recranetConfig = <?php echo json_encode($recranetConfig); ?>;
 
-    (function(d, random) {
-        var l = d.createElement('link'); l.rel = 'stylesheet'; l.type = 'text/css';
-        l.href = 'https://recranet.com/sdk/sdk.css?' + random + '=' + random + '&organization=' + recranetConfig.organization;
-        var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true;
-        s.src = 'https://recranet.com/sdk/sdk.js?' + random + '=' + random;
-        var h = d.getElementsByTagName('head')[0]; h.appendChild(l); h.appendChild(s);
-    })(document, (new Date()).getTime());
-</script>
+        (function(d, random) {
+            var l = d.createElement('link'); l.rel = 'stylesheet'; l.type = 'text/css';
+            l.href = 'https://recranet.com/sdk/sdk.css?' + random + '=' + random + '&organization=' + recranetConfig.organization;
+            var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true;
+            s.src = 'https://recranet.com/sdk/sdk.js?' + random + '=' + random;
+            var h = d.getElementsByTagName('head')[0]; h.appendChild(l); h.appendChild(s);
+        })(document, (new Date()).getTime());
+    </script>
+<?php endif; ?>
