d1ae90a5264917184588343ee557ac9fa23c85a1
Mike Pagé <mike@recranet.com>
2016-07-12 10:43:49 +0200

    Update Joomla mod recranet

--- mod_recranet.php
+++ mod_recranet.php
@@ -17,4 +17,10 @@ $recranetConfig = array(
     'organization' => (int) $params->get('organization')
 );
 
+// Set base URL for Angular HTML5 Mode routing
+if ((boolean) $params->get('html5Mode')) {
+    $link = JFactory::getApplication()->getMenu()->getActive()->link;
+    JFactory::getDocument()->setBase(JRoute::_($link) . '/');
+}
+
 require JModuleHelper::getLayoutPath('mod_recranet', $params->get('layout', 'default'));
