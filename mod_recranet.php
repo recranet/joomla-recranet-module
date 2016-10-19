7b47b52a495587ddab5499fef3d46b9fc1be1da8
Thijs Clowting <thijs@clowting.me>
2016-10-19 14:46:57 +0200

    Add sync/async option, improved field style

--- mod_recranet.php
+++ mod_recranet.php
@@ -27,25 +27,37 @@ if ($params->get('html5Mode')) {
     $document->setBase(JRoute::_($link) . '/');
 }
 
-// Add jQuery to head
-if ($params->get('jquery')) {
-    $document->addScript('https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js');
-}
-
 // Add SDK loader to head
 if ($params->get('sdk')) {
-    $javascript = "" .
-        "var recranetConfig = %s;" . PHP_EOL .
+    if ($params->get('async')) {
+        // Asynchronously
+        $javascript = "" .
+            "var recranetConfig = %s;" . PHP_EOL .
 
-        "(function(d, random) {" . PHP_EOL .
-        "    var l = d.createElement('link'); l.rel = 'stylesheet'; l.type = 'text/css';" . PHP_EOL .
-        "    l.href = 'https://recranet.com/sdk/sdk.css?' + random + '=' + random + '&organization=' + recranetConfig.organization;" . PHP_EOL .
-        "    var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true;" . PHP_EOL .
-        "    s.src = 'https://recranet.com/sdk/sdk.js?' + random + '=' + random;" . PHP_EOL .
-        "    var h = d.getElementsByTagName('head')[0]; h.appendChild(l); h.appendChild(s);" . PHP_EOL .
-        "})(document, (new Date()).getTime());";
+            "(function(d, random) {" . PHP_EOL .
+            "    var l = d.createElement('link'); l.rel = 'stylesheet'; l.type = 'text/css';" . PHP_EOL .
+            "    l.href = 'https://recranet.com/sdk/sdk.css?' + random + '=' + random + '&organization=' + recranetConfig.organization;" . PHP_EOL .
+            "    var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true;" . PHP_EOL .
+            "    s.src = 'https://recranet.com/sdk/sdk.js?' + random + '=' + random;" . PHP_EOL .
+            "    var h = d.getElementsByTagName('head')[0]; h.appendChild(l); h.appendChild(s);" . PHP_EOL .
+            "})(document, (new Date()).getTime());";
 
-    $document->addScriptDeclaration(sprintf($javascript, json_encode($recranetConfig)));
+        $document->addScriptDeclaration(sprintf($javascript, json_encode($recranetConfig)));
+    } else {
+        // In sync
+        $date = new DateTime();
+        $randomString = $date->format('U') . "=" . $date->format('U');
+        $organizationString = "&organization=" . $recranetConfig['organization'];
+
+        $scriptHtml = "" .
+            "<script type='text/javascript'>" . PHP_EOL .
+            "   var recranetConfig = %s;" . PHP_EOL .
+            "</script>" . PHP_EOL .
+            "<script src='https://recranet.com/sdk/sdk.js?%s' type='text/javascript'></script>";
+
+        $document->addCustomTag(vsprintf($scriptHtml, [json_encode($recranetConfig), $randomString]));
+        $document->addStylesheet('https://recranet.com/sdk/sdk.css?' . $randomString . $organizationString);
+    }
 }
 
 require JModuleHelper::getLayoutPath('mod_recranet', $params->get('layout', 'default'));
