a596d522e0b44afc28c427d9f4c3dff41fe46323
Mike Pagé <mikepage@users.noreply.github.com>
2017-01-19 10:53:45 +0100

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -6,11 +6,14 @@
 defined('_JEXEC') or die;
 
 $initializer = $params->get('enable_initializer', false) ? ' recranet-container-init' : '';
-$action = $params->get('action', false) ? ' action="' . $params->get('action', '') . '"' : '';
+$suffix = '';
 
+if ($action = $params->get('action') && !empty($action)) {
+    $suffix = ' action="' . $params->get('action', '') . '"';
+}
 ?>
 <div class="recranet-container<?php echo $initializer; ?>"<?php echo $initializer; ?> data-eq-pts="small: <?php echo $params->get('breakpointSmall'); ?>, medium: <?php echo $params->get('breakpointMedium'); ?>, large: <?php echo $params->get('breakpointLarge'); ?>">
     <?php
-        echo '<recranet-' . $params->get('view') . $action . '></recranet-' . $params->get('view') . '>';
+        echo '<recranet-' . $params->get('view') . $suffix . '></recranet-' . $params->get('view') . '>';
     ?>
 </div>
